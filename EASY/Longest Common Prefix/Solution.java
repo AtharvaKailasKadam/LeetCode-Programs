98091be8e9ac69ef4105bd13f4c1441148405493
Your Name <atharvakailaskadam1809@gmail.com>
2025-09-03 22:10:41 +0530

    Done and Dusted with Another LeetCode Problem

---
+++ EASY/Longest Common Prefix/Solution.java
@@ -0,0 +1,35 @@
+class Solution
+{
+    public String longestCommonPrefix(String[] strs)
+    {
+        if(strs.length == 0 || strs == null)
+        {
+            return "String Empty";
+        }
+
+        String prefix = strs[0];
+
+        for(int i = 0; i < strs.length; i++)
+        {
+            while(strs[i].indexOf(prefix) != 0)
+            {
+                prefix = prefix.substring(0, prefix.length() - 1);
+            }
+            if(prefix.isEmpty())
+            {
+                return "";
+            }
+        }
+        return prefix;
+    }
+    public static void main(String[] args)
+    {
+        String[] strs1 = {"flower", "flow", "flight"};
+        String[] strs2 = {"dog", "racecar", "car"};
+
+        Solution sol = new Solution();
+        sol.longestCommonPrefix(strs1);
+        sol.longestCommonPrefix(strs2);
+
+    }
+}
